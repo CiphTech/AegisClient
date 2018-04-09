188f39939569709c827ed0835828e45f05aa6d66
RumbaInkanta <RUMBAINKANTA@gmail.com>
2018-04-09 07:00:35 +0300

    добавил check-in.component.ts начат routing, возможно вместо app.component.ts придется ставить дополнительный компонент потому что routr-outlet другие компоненты не скрывает

--- AegisApp/src/app/app.module.ts
+++ AegisApp/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule }   from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { ConversationComponent } from './conversation/conversation.component';
 import { DialogComponent } from './dialog/dialog.component';
+import { CheckInComponent } from './check-in/check-in.component';
 
+const appRoutes: Routes =[
+    { path: 'checkin', component: CheckInComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ConversationComponent,
-    DialogComponent
+    DialogComponent,
+    CheckInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

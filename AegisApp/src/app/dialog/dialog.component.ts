403e69b62a606ef2f3d918f5f6bb32d7f8cf9088
RumbaInkanta <RUMBAINKANTA@gmail.com>
2018-04-27 23:12:32 +0300

    Подключение утилиты по string, реакция на пустые поля ввода

--- AegisApp/src/app/dialog/dialog.component.ts
+++ AegisApp/src/app/dialog/dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Conversation, Message, Account} from '../model';
 import { AuthService } from '../auth.service';
+import { StringHelper } from '../utility';
 
 @Component({
   selector: 'app-dialog',
@@ -11,7 +12,9 @@ export class DialogComponent implements OnInit {
 
 	textMessage:string;	
   	convSvc: AuthService;
-	selectedConv:Conversation = new Conversation("111");
+	selectedConv:Conversation = new Conversation("default");
+	selectTrue: boolean = false; // для выделения message при выборе
+	stringNull: boolean = false; // для отображения ошибки ввода
 
   constructor(convSvc: AuthService) {
     this.convSvc = convSvc;
@@ -22,15 +25,23 @@ export class DialogComponent implements OnInit {
 
   selectConv(conv: Conversation){
   	this.selectedConv = conv;
+  	this.selectTrue = true;
   }
 
   sendMessage (){
+  	if (StringHelper.isNullOrEmpty(this.textMessage)) {
+  		this.stringNull = true;
+  		return;
+  	}
+
   	let msg = new Message(this.selectedConv.nameConv,this.textMessage,"from");
   	this.selectedConv.addMessage(msg);
   	this.textMessage="";
+  	this.stringNull = false;
   }
 
-  conver(){
-  	console.log(this.convSvc.conversations);
+  addConv (acc: Account, nameConv:string){
+  	let conv = new Conversation(nameConv);
+  	acc.addConv(conv);
   }
 }

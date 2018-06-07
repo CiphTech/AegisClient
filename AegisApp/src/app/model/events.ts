e29505aaaa95c0d6c36a59d30e2d86635159ae3d
Chusiksmirnov <chusiksmirnov@hotmail.com>
2018-06-08 01:16:16 +0300

    events.ts has been added

---
+++ AegisApp/src/app/model/events.ts
@@ -0,0 +1,57 @@
+class AegisEvent<T> {
+
+	private _callbacks: IEventHandlerDesc<T>[] = [];
+
+	public subscribe(handler: IEventHandler<T>, subscriber: any): ITypedSubscription<T> {
+
+		let self = this;
+
+		let res: ITypedSubscription<T> = {
+			handler: handler,
+			evt: self,
+			unsubscribe: function() { self.unsubscribe(handler); }
+		}
+
+		this._callbacks.push({ handler: handler, subscriber: subscriber });
+
+		return res;
+	}
+
+	public unsubscribe(handler: IEventHandler<T>): void {
+
+		let callbacks: IEventHandlerDesc<T>[] = [];
+
+		for(let i = 0; i < this._callbacks.length; i++){
+			if (this._callbacks[i].handler !== handler)
+				callbacks.push(this._callbacks[i]);
+		}
+
+		this._callbacks = callbacks;
+	}
+
+	public raise: IEventHandler<T> = function(arg: T, context?: any){
+		let callbacks: IEventHandlerDesc<T>[] = this._callbacks;
+
+		for (let i = 0; i < callbacks.length; i++){
+			callbacks[i].handler.apply(callbacks[i].subscriber, [arg, context]);
+		}
+	}
+}
+
+interface ISubscription {
+	unsubscribe(): void;
+}
+
+interface ITypedSubscription<T> extends ISubscription {
+	handler: IEventHandler<T>;
+	evt: AegisEvent<T>;
+}
+
+interface IEventHandler<T> {
+	(arg: T, context?: any);
+}
+
+interface IEventHandlerDesc<T> {
+	handler: IEventHandler<T>;
+	subscriber: any;
+}

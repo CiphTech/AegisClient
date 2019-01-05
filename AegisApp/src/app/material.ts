ba494b28e185951664ca3b5bebdba861693e3eb4
RumbaInkanta <RUMBAINKANTA@gmail.com>
2019-01-05 12:37:08 +0300

    add badge

--- AegisApp/src/app/material.ts
+++ AegisApp/src/app/material.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatToolbarModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatToolbarModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatToolbarModule, MatBadgeModule, 
+  MatInputModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatToolbarModule, MatBadgeModule, 
+  MatInputModule],
 })
 export class MaterialModule { }
